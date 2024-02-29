82e5d11e3fbc9ba3fdcd6f1a72a6dff85163edb4
Akande Sulaimon Olamilekan <122680161+kingAkande@users.noreply.github.com>
2024-02-29 12:04:32 +0100

    Update Lock.js

--- test/Lock.js
+++ test/Lock.js
@@ -5,31 +5,29 @@ const {
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
-describe("Lock", function () {
+describe("eventEmit", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+   
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+
 
     // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners();
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const evenEmit = await ethers.getContractFactory(evenEmit");
+    const evenEmit = await Lock.deploy(unlockTime, { value: lockedAmount });
 
     return { lock, unlockTime, lockedAmount, owner, otherAccount };
   }
 
   describe("Deployment", function () {
     it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+      const { evenEmit, unlockTime } = await loadFixture(deployOneYearLockFixture);
 
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+      expect(await evenEmit.unlockTime()).to.equal(unlockTime);
     });
 
     it("Should set the right owner", async function () {
@@ -39,11 +37,11 @@ describe("Lock", function () {
     });
 
     it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
+      const { evenEmit, lockedAmount } = await loadFixture(
         deployOneYearLockFixture
       );
 
-      expect(await ethers.provider.getBalance(lock.target)).to.equal(
+      expect(await ethers.provider.getBalance(levenEmit.target)).to.equal(
         lockedAmount
       );
     });
@@ -63,13 +61,13 @@ describe("Lock", function () {
       it("Should revert with the right error if called too soon", async function () {
         const { lock } = await loadFixture(deployOneYearLockFixture);
 
-        await expect(lock.withdraw()).to.be.revertedWith(
+        await expect(levenEmit.withdraw()).to.be.revertedWith(
           "You can't withdraw yet"
         );
       });
 
       it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
+        const { evenEmit, unlockTime, otherAccount } = await loadFixture(
           deployOneYearLockFixture
         );
 
@@ -83,7 +81,7 @@ describe("Lock", function () {
       });
 
       it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
+        const { evenEmit, unlockTime } = await loadFixture(
           deployOneYearLockFixture
         );
 
@@ -96,14 +94,14 @@ describe("Lock", function () {
 
     describe("Events", function () {
       it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
+        const {evenEmit, unlockTime, lockedAmount } = await loadFixture(
           deployOneYearLockFixture
         );
 
         await time.increaseTo(unlockTime);
 
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
+        await expect(evenEmit.withdraw())
+          .to.emit(levenEmit, "Withdrawal")
           .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
       });
     });
@@ -116,8 +114,8 @@ describe("Lock", function () {
 
         await time.increaseTo(unlockTime);
 
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
+        await expect(evenEmit.withdraw()).to.changeEtherBalances(
+          [owner, evenEmit],
           [lockedAmount, -lockedAmount]
         );
       });
